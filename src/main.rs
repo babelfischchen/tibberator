752d13e5b5aed343b08b85c66f88076a1cc64ae7
babelfischchen <161168789+babelfischchen@users.noreply.github.com>
2024-02-25 22:43:47 +0100

    End immediately on user shutdown
    Reworked loop structure so that the user can immediately exit the application without having to wait for the next data.
    Also added support for reconnecting after lost connection.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ use ctrlc::set_handler;
 use exitcode;
 use futures::{executor::block_on, stream::StreamExt};
 use graphql_ws_client::{graphql::StreamingOperation, Subscription};
+use rand::Rng;
+use tokio::time;
 
 use tibberator::tibber::{
     get_live_measurement, live_measurement::LiveMeasurementLiveMeasurement, AccessConfig,
@@ -40,29 +42,22 @@ async fn main() {
     })
     .expect("Error setting CTRL+C handler");
 
-    let mut subscription = block_on(get_live_measurement(&config));
+    let subscription = block_on(subscription_loop(config, receiver));
 
-    {
-        match subscription {
-            Ok(ref mut result) => {
-                println!("Connection established");
-                block_on(subscription_loop(result, receiver))
-            }
-            Err(error) => {
-                println!("{:?}", error);
-                std::process::exit(exitcode::PROTOCOL);
-            }
-        };
-    }
     execute!(stdout(), LeaveAlternateScreen).unwrap();
 
-    let stop_result = block_on(subscription.unwrap().stop());
-    match stop_result {
-        Ok(()) => std::process::exit(exitcode::OK),
-        Err(error) => {
-            println!("{:?}", error);
-            std::process::exit(exitcode::PROTOCOL)
+    match subscription {
+        Some(value) => {
+            let stop_result = block_on(value.stop());
+            match stop_result {
+                Ok(()) => std::process::exit(exitcode::OK),
+                Err(error) => {
+                    println!("{:?}", error);
+                    std::process::exit(exitcode::PROTOCOL)
+                }
+            };
         }
+        _ => {}
     };
 }
 
@@ -132,31 +127,119 @@ fn print_screen(data: LiveMeasurementLiveMeasurement) {
     stdout().flush().unwrap();
 }
 
-async fn subscription_loop(
+fn check_user_shutdown(receiver: &Receiver<bool>) -> bool {
+    let received_value = receiver.recv_timeout(Duration::from_millis(100));
+    match received_value {
+        Ok(value) => value,
+        Err(error) => match error {
+            RecvTimeoutError::Timeout => false,
+            RecvTimeoutError::Disconnected => {
+                println!("{:?}", error);
+                true
+            }
+        },
+    }
+}
+
+async fn connect_live_measurement(
+    config: &AccessConfig,
+) -> Option<Subscription<StreamingOperation<LiveMeasurement>>> {
+    let subscription = get_live_measurement(&config).await;
+
+    match subscription {
+        Ok(result) => {
+            println!("Connection established");
+            Some(result)
+        }
+        Err(error) => {
+            println!("{:?}", error);
+            std::process::exit(exitcode::PROTOCOL);
+        }
+    }
+}
+
+async fn loop_for_data(
     subscription: &mut Subscription<StreamingOperation<LiveMeasurement>>,
+    receiver: &Receiver<bool>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut loop_counter = 0;
+    loop {
+        let next_data = time::timeout(Duration::from_secs(1), subscription.next()).await;
+        match next_data {
+            Ok(item) => {
+                let current_state = item
+                    .unwrap()
+                    .unwrap()
+                    .data
+                    .unwrap()
+                    .live_measurement
+                    .unwrap();
+                print_screen(current_state);
+            }
+            Err(_) => {
+                loop_counter += 1;
+                if loop_counter > 120 {
+                    return Ok(());
+                }
+            }
+        }
+
+        if check_user_shutdown(&receiver) == true {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Interrupted,
+                "Shutdown requested",
+            )));
+        }
+    }
+}
+
+async fn subscription_loop(
+    config: AccessConfig,
     receiver: Receiver<bool>,
-) {
+) -> Option<Box<Subscription<StreamingOperation<LiveMeasurement>>>> {
+    let mut subscription = Box::new(
+        connect_live_measurement(&config)
+            .await
+            .expect("Connection to Tibber could not be established."),
+    );
+
     write!(stdout(), "Waiting for data ...").unwrap();
     stdout().flush().unwrap();
 
-    while let Some(item) = subscription.next().await {
-        let current_state = item.unwrap().data.unwrap().live_measurement.unwrap();
-        print_screen(current_state);
-
-        let received_value = receiver.recv_timeout(Duration::from_millis(100));
-        match received_value {
-            Ok(value) => {
-                if value == true {
-                    break;
-                }
-            }
-            Err(error) => match error {
-                RecvTimeoutError::Timeout => {}
-                RecvTimeoutError::Disconnected => {
+    loop {
+        let final_result = loop_for_data(subscription.as_mut(), &receiver).await;
+        match final_result {
+            Ok(_) => {
+                if let Err(error) = subscription.stop().await {
                     println!("{:?}", error);
-                    break;
+                    std::process::exit(exitcode::PROTOCOL)
+                };
+
+                let number_of_seconds = rand::thread_rng().gen_range(1..=60);
+                println!(
+                    "\nConnection lost, waiting for {}s before reconnecting.",
+                    number_of_seconds
+                );
+
+                for _ in 0..=number_of_seconds {
+                    if check_user_shutdown(&receiver) == true {
+                        return None;
+                    }
+                    std::thread::sleep(time::Duration::from_secs(1));
                 }
-            },
-        };
+
+                subscription = Box::new(
+                    connect_live_measurement(&config)
+                        .await
+                        .expect("Connection to Tibber could not be established."),
+                );
+            }
+            Err(error) => {
+                println!("\n{:?}", error);
+                break;
+            }
+        }
     }
+
+    Some(subscription)
 }
